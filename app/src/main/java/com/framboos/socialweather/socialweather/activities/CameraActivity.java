0fc9c5aa27977ff03807543c6297e55bf4f30281
Mark Nijboer <mark@HackBook-Pro.local>
2016-06-09 23:26:54 +0200

    Added Camera feature

---
+++ app/src/main/java/com/framboos/socialweather/socialweather/activities/CameraActivity.java
@@ -0,0 +1,19 @@
+package com.framboos.socialweather.socialweather.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.framboos.socialweather.socialweather.R;
+
+/**
+ * Created by mark on 09/06/16.
+ */
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_view);
+    }
+
+}
--- app/src/main/java/com/framboos/socialweather/socialweather/fragments/IntroFragment.java
+++ app/src/main/java/com/framboos/socialweather/socialweather/fragments/IntroFragment.java
@@ -1,10 +1,13 @@
 package com.framboos.socialweather.socialweather.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.framboos.socialweather.socialweather.R;
 
@@ -13,8 +16,24 @@ public class IntroFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.intro_view, container, false);
+
+        Button uploadButton = (Button) view.findViewById(R.id.action_upload);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.v("IntoFragment","Click detected");
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
         // Return an instance of the intro view so it'll be the root view for this fragment
-        return inflater.inflate(R.layout.intro_view, container, false);
+        return view;
     }
+
+
 
 }
